601bb55f4136c5763e16f7326a0081f2ca858a6c
Bernardo Soares <soaresbernardo1406@gmail.com>
2025-11-05 09:26:08 -0300

    first commit

---
+++ src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.calculo;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme f) {
+//        tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        this.tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo) {
+      this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
